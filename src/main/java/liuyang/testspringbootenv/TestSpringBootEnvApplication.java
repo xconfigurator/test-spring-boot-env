f0ebee6609a9a5b4ee3d93ed9d162783a5719e9a
xconfigurator <xconfigurator@hotmail.com>
2021-03-01 23:41:07 +0800

    first commit

---
+++ src/main/java/liuyang/testspringbootenv/TestSpringBootEnvApplication.java
@@ -0,0 +1,15 @@
+package liuyang.testspringbootenv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class TestSpringBootEnvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringBootEnvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/liuyang/testspringbootenv/TestSpringBootEnvApplicationTests.java
@@ -0,0 +1,13 @@
+package liuyang.testspringbootenv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootEnvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
