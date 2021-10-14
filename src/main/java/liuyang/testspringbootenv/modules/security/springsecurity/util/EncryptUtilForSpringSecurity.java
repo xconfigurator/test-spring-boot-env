ac6bb42b1a9b4187de23c02d45412f55ab0d39d7
xconfigurator <xconfigurator@hotmail.com>
2021-10-14 20:17:09 +0800

    适配家中工作站环境

---
+++ src/main/java/liuyang/testspringbootenv/modules/security/springsecurity/util/EncryptUtilForSpringSecurity.java
@@ -0,0 +1,10 @@
+package liuyang.testspringbootenv.modules.security.springsecurity.util;
+
+/**
+ * 加密、加盐策略在新用户注册以及验证的时候应保持一致
+ *
+ * @author liuyang
+ * @scine 2021/8/26
+ */
+public class EncryptUtilForSpringSecurity {
+}
