2d2270b8d1cad915c89456ffa5757f25e79b9be4
liuyang <xconfigurator@hotmail.com>
2022-06-20 15:20:04 +0800

    增加同域请求示例。

--- src/main/java/liuyang/testspringbootenv/modules/web/controller/ThymeleafController.java
+++ src/main/java/liuyang/testspringbootenv/modules/web/controller/ThymeleafController.java
@@ -16,12 +16,13 @@ import java.util.List;
  * @author liuyang
  * @scine 2021/7/2
  */
-@Controller
+@Controller// 注意Controller的类型。
 @RequestMapping("/th")
 public class ThymeleafController {
 
     @GetMapping
     public String hello(Model model) {
+        // Model会被放入请求域
         // 注：IntelliJ IDEA 启动maven工程的方式 clean package spring-boot:run -Dmaven.test.skip=true
         model.addAttribute("info", "test Spring Boot DevTools");
         model.addAttribute("username", "liuyang");
